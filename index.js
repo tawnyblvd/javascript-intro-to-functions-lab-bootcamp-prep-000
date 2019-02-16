8130df75d6d21cefd62a2811d5b6827e799ae965
Ariel <cantegone@yahoo.com>
2019-02-16 19:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-// function logShout(string) {
-//   console.log(string.toUpperCase);
-// }
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
 
 // function logWhisper(string) {
 //   console.log(string.toLowerCase);
